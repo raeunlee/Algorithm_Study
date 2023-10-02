049d6ff685b79d7f625d50b887ff60f14bf26142
raeunlee <raeun19@naver.com>
2023-10-03 00:52:33 +0900

    feat: 10.02 two sol

---
+++ raeunlee/week_11/BOJ_2606_바이러스.py
@@ -0,0 +1,42 @@
+from collections import deque
+
+n = int(input())
+m = int(input())
+
+coms = [[] for _ in range(n+1)]
+visited = [False] * (n+1)
+cnt = 0
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    coms[a].append(b)
+    coms[b].append(a)
+
+
+def dfs(start):
+    global cnt
+    visited[start] = True
+    for i in coms[start]:
+        if visited[i] == False:
+            dfs(i)
+            cnt += 1
+
+dfs(1)
+print(cnt)
+
+# def bfs(coms, c):
+#     global cnt
+#     q = deque([c]) 
+#     while q:
+#         pop = q.popleft()
+#         visited[pop] = True
+#         for i in coms[pop]:
+#             if visited[i] == False:
+#                 visited[i] = True
+#                 q.append(i)
+#                 cnt += 1
+#     print(cnt)
+
+# bfs(coms, 1)               
+
+        
