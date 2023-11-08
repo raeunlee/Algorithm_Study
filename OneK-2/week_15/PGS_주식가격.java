0c73116d01f2e7b3859dd4cd0e2759217b41292d
OneK-2 <85729858+OneK-2@users.noreply.github.com>
2023-11-08 21:39:53 +0900

    feat: week_15

---
+++ OneK-2/week_15/PGS_주식가격.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int len = prices.length;
+        int[] answer = new int[len];
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < len; i++) {
+            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                answer[stack.peek()] = i - stack.peek();
+                stack.pop();
+            }
+            stack.push(i);
+        }
+        while (!stack.isEmpty()) {
+            answer[stack.peek()] = len - stack.peek() - 1;
+            stack.pop();
+        }
+
+        return answer;
+    }
+}
