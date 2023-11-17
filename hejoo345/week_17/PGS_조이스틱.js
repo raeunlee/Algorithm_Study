7ca9c71abf205c138aa835ff9795fffb0285d7eb
김희주 <67685741+hejoo345@users.noreply.github.com>
2023-11-18 01:01:13 +0900

    comment: pgs_조이스틱

---
+++ hejoo345/week_17/PGS_조이스틱.js
@@ -0,0 +1,18 @@
+function solution(name) {
+    let answer = 0;
+    let min_move = name.length - 1;
+
+    name.split("").forEach((n, i) => {
+        answer += Math.min(n.charCodeAt() - 65, 91 - n.charCodeAt());
+
+        let idx = i + 1;
+        // 연속되는 A 개수
+        while (idx < name.length && name[idx] == 'A') {
+            idx++;
+        }
+        min_move = Math.min(min_move,
+            i * 2 + name.length - idx,
+            i + 2 * (name.length - idx))
+    });
+    return answer + min_move;
+}
