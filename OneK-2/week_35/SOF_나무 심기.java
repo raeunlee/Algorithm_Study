c50f852ff1f5e9cce952ba72f4ba99aec754991b
이원규 <85729858+OneK-2@users.noreply.github.com>
2024-03-31 16:34:26 +0900

    feat: 35주차 4문제

---
+++ OneK-2/week_35/SOF_나무 심기.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        int max = Math.max(arr[0] * arr[1], arr[n - 1] * arr[n - 2]);
+
+        System.out.println(max);
+    }
+}
